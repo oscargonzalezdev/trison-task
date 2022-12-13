f1a04e605d27d8c5c3f8bcf9f7f369adcd4d9d29
Oscar González <oscargonzalezpena@gmail.com>
2022-12-13 11:00:00 +0100

    feat(home): add semantic tags to render home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import Logo from "../components/Layout/Subcomponents/Logo";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-home">
+      <section className="greeting">
+        <h1>Welcome to</h1>
+        <div className="logo">
+          <Logo size={"4.5rem"} />
+        </div>
+        <p>
+          Are you ready to discover our incredible menu? <br />
+          For any enquiries, please use our contact form
+        </p>
+      </section>
     </div>
   );
 }
