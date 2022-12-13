07a571281966450483df4d54675c9a7c2f41d4d3
Oscar González <oscargonzalezpena@gmail.com>
2022-12-13 10:49:36 +0100

    feat(form): add a background image to the form

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -30,77 +30,79 @@ function Form() {
   };
 
   return (
-    <div className={styles.formContainer}>
-      <h1>Get in touch!</h1>
-      <form className={styles.form} id="form" onSubmit={handleFormSubmit}>
-        <div className={styles.twoColumns}>
-          <label>
-            <span className={styles.req}>First Name *</span>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              required
-              pattern="[a-zA-Z\s]+"
-              title="Please enter your first name using only letters and spaces"
-              minLength={2}
-              maxLength={20}
-            />
-          </label>
-          <label>
-            <span className={styles.req}>Last Name *</span>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              required
-              pattern="[a-zA-Z\s]+"
-              title="Please enter your last name using only letters and spaces"
-              minLength={2}
-              maxLength={20}
-            />
-          </label>
-        </div>
-        <div className={styles.twoColumns}>
-          <label>
-            <span className={styles.req}>Company *</span>
-            <input
-              type="text"
-              id="company"
-              name="company"
-              required
-              minLength={2}
-              maxLength={20}
-            />
-          </label>
-          <label>
-            <span className={styles.req}>Email Address *</span>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              required
-              pattern="[^\s@]+@[^\s@]+\.[^\s@]+"
-              title="Please enter a valid email address"
-            />
-          </label>
-        </div>
-        <div className={styles.enquiry}>
-          <label>
-            <span className={styles.req}>Enquiry*</span>
-            <textarea
-              minLength={5}
-              maxLength={600}
-              id="enquiry"
-              name="enquiry"
-              required
-            ></textarea>
-          </label>
-        </div>
-        <button className="btn" type="submit">
-          Send
-        </button>
-      </form>
+    <div className={styles.bgForm}>
+      <div className={styles.formContainer}>
+        <h1>Get in touch!</h1>
+        <form className={styles.form} id="form" onSubmit={handleFormSubmit}>
+          <div className={styles.twoColumns}>
+            <label>
+              <span className={styles.req}>First Name *</span>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                required
+                pattern="[a-zA-Z\s]+"
+                title="Please enter your first name using only letters and spaces"
+                minLength={2}
+                maxLength={20}
+              />
+            </label>
+            <label>
+              <span className={styles.req}>Last Name *</span>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                required
+                pattern="[a-zA-Z\s]+"
+                title="Please enter your last name using only letters and spaces"
+                minLength={2}
+                maxLength={20}
+              />
+            </label>
+          </div>
+          <div className={styles.twoColumns}>
+            <label>
+              <span className={styles.req}>Company *</span>
+              <input
+                type="text"
+                id="company"
+                name="company"
+                required
+                minLength={2}
+                maxLength={20}
+              />
+            </label>
+            <label>
+              <span className={styles.req}>Email Address *</span>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                pattern="[^\s@]+@[^\s@]+\.[^\s@]+"
+                title="Please enter a valid email address"
+              />
+            </label>
+          </div>
+          <div className={styles.enquiry}>
+            <label>
+              <span className={styles.req}>Enquiry*</span>
+              <textarea
+                minLength={5}
+                maxLength={600}
+                id="enquiry"
+                name="enquiry"
+                required
+              ></textarea>
+            </label>
+          </div>
+          <button className="btn" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
